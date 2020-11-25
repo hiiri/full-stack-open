80c6e33b06d8bdd227988fdcc3290f80864e2972
Aleksi Cohen <alcohen@student.hive.fi>
2020-11-25 20:22:35 +0200

    component

--- part2/rest-countries/src/App.js
+++ part2/rest-countries/src/App.js
@@ -1,64 +1,7 @@
 import {React, useEffect, useState} from 'react'
 import axios from 'axios'
 
-const Countries = ({ countries, filter }) => {
-
-  const filteredCountriesId = (Object.keys(countries).filter(
-    countryId => countries[countryId].name.toLowerCase()
-    .includes(filter.toLowerCase())
-  ))
-
-  if (filteredCountriesId.length > 10) {
-    return (
-      <div>
-        <p>Too many matches, specify another filter</p>
-      </div>
-    )
-  }
-  else if (filteredCountriesId.length > 1) {
-    return (
-      <div>
-        {
-          filteredCountriesId.map(countryId =>
-            <p key={countryId}>
-              {countries[countryId].name}
-            </p>
-          )
-        }
-      </div>
-    )
-  }
-  else if (filteredCountriesId.length === 1) {
-    const country = countries[filteredCountriesId[0]]
-    console.log(country)
-    return (
-      <div>
-        <h1>{country.name}</h1>
-        <p>
-          capital {country.capital}
-          <br />
-          population {country.population}
-        </p>
-        <h2>languages</h2>
-        <ul>
-          {country.languages.map(lang =>
-            <li key={lang.name}>
-              {lang.name}
-            </li>
-          )}
-        </ul>
-        <img src={country.flag} width="128px" alt={`flag of ${country.name}`}></img>
-      </div>
-    )
-  }
-  else {
-    return (
-      <div>
-        <p>No matches found</p>
-      </div>
-    )
-  }
-}
+import Countries from './components/Countries'
 
 const App = () => {
 
